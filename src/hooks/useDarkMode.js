7afdeac8d01594d0e2d59553164c471559a02855
nathan-saygers <nathan.saygers@gmail.com>
2019-12-04 14:36:25 -0800

    got the slider working, but local storage persist still not working

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,18 @@
 import useLocalStorage from './useLocalStorage';
+import {useEffect} from 'react';
 
-const useDarkMode = () => {
-  const [storedValue, setStoredValue] = useLocalStorage(darkOn, false)
+const useDarkMode = (initialValue) => {
+  const [storedValue, setStoredValue, setValue] = useLocalStorage('darkOn', initialValue)
+  useEffect(() => {
+    let body = document.getElementsByTagName('body');
+    console.log(body);
+    if(storedValue){
+      body[0].className = 'dark-mode';
+    } else {
+      body[0].className = ('');
+    }
+  }, [storedValue]);
+
   return [storedValue, setStoredValue];
 }
 
